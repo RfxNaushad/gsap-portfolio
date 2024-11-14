e30a208b0b580af5a507aa79e9f15216aad38d95
storngfornt <au0038173@gmail.com>
2024-11-14 12:39:52 +0600

    latest

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ export default function Footer() {
           >
             <SocialLink
               platform="Up work"
-              username="@jahid"
+              username="@mamunur"
               icon={<FaSquareUpwork className="w-8 h-8" />}
             />
           </a>
