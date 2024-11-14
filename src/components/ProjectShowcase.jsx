544f8211458ae7a1aa2822a899ffa5e041dc7bd1
storngfornt <au0038173@gmail.com>
2024-11-14 12:38:18 +0600

    updated

--- src/components/ProjectShowcase.jsx
+++ src/components/ProjectShowcase.jsx
@@ -8,8 +8,12 @@ import Card from "./Card";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+// import "swiper/css/pagination";
 import "swiper/css/navigation";
-
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
 
 // Register ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
@@ -19,39 +23,63 @@ export default function ProjectShowcase() {
   const projects = [
     {
       id: 1,
-      title: "Oh She Glows",
-      link:"https://ohsheglows.com/",
+      title: "Lugz",
+      link: "https://lugz.com/",
       image: "/recent_project1.png",
     },
     {
       id: 2,
-      title: "Invisible Children",
-      link:"https://invisiblechildren.com/",
+      title: "Playstation",
+      link: "https://blog.playstation.com/",
       image: "/recent_project2.png",
     },
     {
       id: 3,
-      title: "Usain Bolt",
-      link:"https://usainbolt.com/",
+      title: "Pinchofyum",
+      link: "https://pinchofyum.com/",
       image: "/recent_project3.png",
     },
     {
       id: 4,
-      title: "10up",
-      link:"https://10up.com/",
+      title: "Walnut",
+      link: "https://www.walnut.io/",
       image: "/recent_project4.png",
     },
     {
       id: 5,
-      title: "Fortmatic",
-      link:"https://fortmatic.com/",
+      title: "Academyselfdefense",
+      link: "https://academyselfdefense.com/",
       image: "/recent_project5.png",
     },
     {
       id: 6,
-      title: "Customer",
-      link:"https://customer.io/",
+      title: "Eden-grow",
+      link: "hhttps://www.eden-grow.de/",
       image: "/recent_project6.png",
+    },
+    {
+      id: 7,
+      title: "Bilberrry",
+      link: "https://bilberrry.com/",
+      image: "/recent_project7.png",
+    },
+    {
+      id: 8,
+      title: "Skysys",
+      link: "https://myskysys.com/",
+      image: "/recent_project8.png",
+    },
+    {
+      id: 9,
+      title: "Sacballet",
+      link: "https://www.sacballet.org/",
+      image: "/recent_project9.png",
+    },
+    {
+      id: 10,
+      title: "WebXd",
+      link: "https://webxd.net/",
+      image: "/recent_project10.png",
     },
   ];
   return (
@@ -78,22 +106,45 @@ export default function ProjectShowcase() {
               slidesPerView={1}
               // pagination= {{clickable:true}}
               loop={true}
-              speed={2000}
+              speed={1500}
               autoplay={{
-                delay: 200,
+                delay: 500,
                 disableOnInteraction: true,
               }}
               // disableOnInteraction={true}
-              navigation={true}
-              modules={[Autoplay, Pagination,Navigation]}
+              navigation={{
+                nextEl: ".custom-next",
+                prevEl: ".custom-prev",
+              }}
+              modules={[Autoplay, Pagination, Navigation]}
             >
               {projects.map((card, index) => (
                 <SwiperSlide key={index}>
                   <div className="cell">
-                    <Card title={card.title} image={card.image} link={card.link} />
+                    <Card
+                      title={card.title}
+                      link={card.link}
+                      image={card.image}
+                    />
                   </div>
                 </SwiperSlide>
               ))}
+
+              {/* Custom Navigation Buttons */}
+              <div className="flex  gap-4 items-center justify-center ">
+                <div className="custom-prev text-[#4ade80] rounded-full">
+                  <IoIosArrowDropleftCircle
+                    className="cursor-pointer"
+                    size={40}
+                  />
+                </div>
+                <div className="custom-next text-[#4ade80] rounded-full">
+                  <IoIosArrowDroprightCircle
+                    className="cursor-pointer"
+                    size={40}
+                  />
+                </div>
+              </div>
             </Swiper>
           </div>
         </div>
